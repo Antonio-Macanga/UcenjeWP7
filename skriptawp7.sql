be0664ab0d8fe9f11f676e1bdd0c36601853f507
Antonio-Macanga <149479502+Antonio-Macanga@users.noreply.github.com>
2024-11-27 20:37:24 +0100

    Završetak kreiranja svih tablica

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,7 +1,41 @@
---create database edunovaWP7
+use master
+go
 
---use edunovaWP7
+drop database if exists edunovawp7;
+go
 
-create table smjer(
-naziv varchar (50)
+create database edunovawp7;
+go
+
+use edunovawp7;
+go
+
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+trajanje int,
+cijena decimal(18,2),
+vaucer bit,
+izvodiseod datetime
 );
+
+create table polaznici(
+sifra int,
+ime varchar(50),
+prezime varchar(50),
+oib char(11),
+email varchar(100)
+);
+
+create table grupe(
+sifra int,
+naziv varchar(20),
+smjer int,
+predavac varchar(50)
+);
+
+create table clanovi(
+grupa int,
+polaznik int
+);
+
