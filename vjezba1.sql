5560822b2af28265d6b66681f0382c8deea46642
Antonio-Macanga <149479502+Antonio-Macanga@users.noreply.github.com>
2024-12-02 19:58:22 +0100

    Vjezba1

--- vjezba1.sql
+++ vjezba1.sql
@@ -1,7 +1,23 @@
---create database posao;
+use master;
+go
+drop database if exists vjezba1;
+go
+create database vjezba1;
+go
+use vjezba1;
+go
 
---use posao
+create table ispitniRok(
+sifra int not null primary key identity(1,1),
+predmet varchar(50) not null,
+vrstaIspita varchar(25) not null,
+datum datetime,
+pristupio varchar(20) not null
+);
 
-create table pozicija(
-naziv varchar(50)
+create table pristupnici(
+ispitniRok int references ispitniRok(sifra) not null,
+student varchar(50) not null,
+brojBodova int not null,
+ocjena int not null
 );
