79074b674e3fdad81ba91944475465ed7ec004d0
Antonio-Macanga <149479502+Antonio-Macanga@users.noreply.github.com>
2024-12-09 20:14:13 +0100

    Maknio šuškavce

--- Zavrsni Rad Borbeni Avioni.sql
+++ Zavrsni Rad Borbeni Avioni.sql
@@ -7,29 +7,29 @@ go
 use borbeniAvioni;
 go
 
-create table država(
+create table drzava(
 sifra int not null primary key identity(1,1),
 naziv varchar (50) not null
 );
 
-create table proizvođač(
+create table proizvodac(
 sifra int not null primary key identity(1,1),
 naziv varchar (50) not null,
-država int not null references država(sifra),
+drzava int not null references drzava(sifra),
 datumOsnivanja datetime
 );
 
 create table avion(
 sifra int not null primary key identity(1,1),
 naziv varchar (50) not null,
-proizvođač int not null references proizvođač(sifra),
+proizvodac int not null references proizvodac(sifra),
 klasa varchar (50) not null,
 datumPrvogLeta datetime not null,
 model varchar (25),
 export bit
 );
 
-insert into država
+insert into drzava
 (naziv) values
 -- 1
 ('Sjedinjene Američke Države'),
@@ -38,14 +38,14 @@ insert into država
 -- 3
 ('Francuska');
 
-insert into proizvođač
-(naziv, država, datumOsnivanja) values
+insert into proizvodac
+(naziv, drzava, datumOsnivanja) values
 ('Lockheed Martin',1,'1995-03-15'),
 ('Suhoj',2, null),
 ('Dassault Avijacija',3, null);
 
 insert into avion
-(naziv, proizvođač, klasa, datumPrvogLeta) values
+(naziv, proizvodac, klasa, datumPrvogLeta) values
 ('F-35 Lightning',1, 'višenamjenski borbeni zrakoplov', '2006-12-15'),
 ('Su-27 Flanker',2, 'višenamjenski borbeni zrakoplov', '1977-05-20'),
 ('Dassault Rafale',3, 'višenamjenski borbeni zrakoplov', '1991-05-19');
