69950480e835331d4844afa874d9e1c1460c2428
Antonio-Macanga <149479502+Antonio-Macanga@users.noreply.github.com>
2024-12-09 20:34:37 +0100

    ZavrsniSQL

--- ZavrsniRadBorbeniAvioni.sql
+++ ZavrsniRadBorbeniAvioni.sql
@@ -7,29 +7,29 @@ go
 use borbeniAvioni;
 go
 
-create table drzava(
+create table drzave(
 sifra int not null primary key identity(1,1),
 naziv varchar (50) not null
 );
 
-create table proizvodac(
+create table proizvodaci(
 sifra int not null primary key identity(1,1),
 naziv varchar (50) not null,
-drzava int not null references drzava(sifra),
+drzava int not null references drzave(sifra),
 datumOsnivanja datetime
 );
 
-create table avion(
+create table avioni(
 sifra int not null primary key identity(1,1),
 naziv varchar (50) not null,
-proizvodac int not null references proizvodac(sifra),
+proizvodac int not null references proizvodaci(sifra),
 klasa varchar (50) not null,
 datumPrvogLeta datetime not null,
 model varchar (25),
 export bit
 );
 
-insert into drzava
+insert into drzave
 (naziv) values
 -- 1
 ('Sjedinjene Američke Države'),
@@ -38,13 +38,13 @@ insert into drzava
 -- 3
 ('Francuska');
 
-insert into proizvodac
+insert into proizvodaci
 (naziv, drzava, datumOsnivanja) values
 ('Lockheed Martin',1,'1995-03-15'),
 ('Suhoj',2, null),
 ('Dassault Avijacija',3, null);
 
-insert into avion
+insert into avioni
 (naziv, proizvodac, klasa, datumPrvogLeta) values
 ('F-35 Lightning',1, 'višenamjenski borbeni zrakoplov', '2006-12-15'),
 ('Su-27 Flanker',2, 'višenamjenski borbeni zrakoplov', '1977-05-20'),
